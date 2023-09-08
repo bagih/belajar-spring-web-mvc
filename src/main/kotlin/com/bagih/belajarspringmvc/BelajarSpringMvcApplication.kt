743724a989703d0fc0795ba46f95f49e83ad678a
Bagi Hartawan <hartawanbagie@gmail.com>
2023-09-08 19:45:09 +0800

    initial commit

---
+++ src/main/kotlin/com/bagih/belajarspringmvc/BelajarSpringMvcApplication.kt
@@ -0,0 +1,11 @@
+package com.bagih.belajarspringmvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BelajarSpringMvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<BelajarSpringMvcApplication>(*args)
+}
---
+++ src/test/kotlin/com/bagih/belajarspringmvc/BelajarSpringMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bagih.belajarspringmvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BelajarSpringMvcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
