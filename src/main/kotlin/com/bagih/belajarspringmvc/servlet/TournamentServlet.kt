c52d81ad31b5a7ecc920349f269fdd31f5cb864f
Bagi Hartawan <hartawanbagie@gmail.com>
2023-09-10 21:22:14 +0800

    Creating TournamentServlet

---
+++ src/main/kotlin/com/bagih/belajarspringmvc/servlet/TournamentServlet.kt
@@ -0,0 +1,27 @@
+package com.bagih.belajarspringmvc.servlet
+
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+
+@WebServlet(urlPatterns = ["/players.do"])
+class TournamentServlet : HttpServlet() {
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        super.doGet(req, resp)
+        resp?.writer?.let { writer ->
+            writer.println("<html>");
+            writer.println(
+                "<head>" +
+                        "<title>Player DB</title>" +
+                        "</head>"
+            )
+            writer.println(
+                "<body>" +
+                        "<H2>Welcome to the Tennis Players database!</H2>" +
+                        "</body>"
+            );
+            writer.println("</html>")
+        }
+    }
+}
